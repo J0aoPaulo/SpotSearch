6b79784043a07bc64e2dfae045849466f03cec85
J0aoPaulo <ojoaopaulo1324@gmail.com>
2024-06-15 22:04:34 -0300

    feat: print albums by type and track number, print albuns with artist name

--- src/main/java/com/J0aoPaulo/SpotSearch/repository/ArtistaRepository.java
+++ src/main/java/com/J0aoPaulo/SpotSearch/repository/ArtistaRepository.java
@@ -10,16 +10,27 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public interface ArtistaRepository extends JpaRepository<Artista, Long> {
 
     @Query("SELECT a.albuns FROM Artista a WHERE LOWER(a.nome) = LOWER(:nome)")
-    Optional<List<Album>> findAllAlbumsByArtistName(@Param("nome") String nome);
+    Optional<List<Album>> getAllAlbumsByArtistName(@Param("nome") String nome);
 
     @Query("SELECT a.musicas FROM Artista a WHERE LOWER(a.nome) = LOWER(:nome)")
-    Optional<List<Musica>> findAllTracksByArtistName(@Param("nome") String nome, Pageable pageable);
+    Optional<List<Musica>> getAllTracksByArtistName(@Param("nome") String nome, Pageable pageable);
 
     @Query("SELECT a.topMusicas FROM Artista a WHERE LOWER(a.nome) = LOWER(:nome)")
-    Optional<List<TopMusica>> findArtistTopMusics(@Param("nome") String nome);
+    Optional<List<TopMusica>> getArtistTopMusics(@Param("nome") String nome);
+
+    @Query("SELECT a FROM Album a WHERE LOWER(a.tipoAlbum) = LOWER(:tipo)")
+    Optional<List<Album>> getAlbunsForType(@Param("tipo") String tipo);
+
+    @Query("SELECT a FROM Album a ORDER BY a.totalFaixas DESC")
+    Optional<List<Album>> getAlbunsTracksDesc();
+
+    @Query(value = "SELECT nome_artista, nome_album " +
+            "FROM artistas JOIN albuns ON artistas.id=albuns.artista_id", nativeQuery = true)
+    Optional<List<Map<String, String>>> getArtistNameAndAlbumsName();
 }
--- src/main/java/com/J0aoPaulo/SpotSearch/ui/UserMenu.java
+++ src/main/java/com/J0aoPaulo/SpotSearch/ui/UserMenu.java
@@ -12,6 +12,7 @@ import com.J0aoPaulo.SpotSearch.service.ConsumoApi;
 import org.springframework.data.domain.PageRequest;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.Scanner;
 
@@ -29,7 +30,10 @@ public class UserMenu {
         SAIR,
         LISTAR_ALBUNS,
         LISTAR_MUSICAS,
-        LISTAR_MUSICAS_POPULARES
+        LISTAR_MUSICAS_POPULARES,
+        LISTAR_ALBUNS_TIPO,
+        LISTAR_ALBUNS_POR_NUMERO_FAIXAS,
+        LISTAR_NOME_ALBUNS
     }
 
     public void menu(String nomeArtista) {
@@ -37,6 +41,9 @@ public class UserMenu {
                     1 - Listar álbuns do artista
                     2 - Listar músicas do artista
                     3 - Listar musicas mais populares
+                    4 - Listar albuns por tipo
+                    5 - Listar album por numero de faixas
+                    6 - Listar nome do artista e seus albuns
                     0 - Sair                                \s
                    \s""";
 
@@ -100,14 +107,14 @@ public class UserMenu {
     private void realizarAcao(OpcaoMenu opcao, String nomeArtista) {
         switch (opcao) {
             case LISTAR_ALBUNS:
-                Optional<List <Album>> albuns = repository.findAllAlbumsByArtistName(nomeArtista);
+                Optional<List <Album>> albuns = repository.getAllAlbumsByArtistName(nomeArtista);
                 albuns.ifPresentOrElse(
                         values -> values.forEach(a -> System.out.println(a.getNomeAlbum())),
                         () -> System.out.println("Nenhum album de " + nomeArtista + "encontrado")
                 );
                 break;
             case LISTAR_MUSICAS:
-                Optional<List<Musica>> musicas = repository.findAllTracksByArtistName
+                Optional<List<Musica>> musicas = repository.getAllTracksByArtistName
                         (nomeArtista, PageRequest.of(0 ,20));
                 musicas.ifPresentOrElse(
                         values -> values.forEach(m -> System.out.println(m.getNome())),
@@ -115,10 +122,36 @@ public class UserMenu {
                 );
                 break;
             case LISTAR_MUSICAS_POPULARES:
-                Optional<List<TopMusica>> topMusicas = repository.findArtistTopMusics(nomeArtista);
+                Optional<List<TopMusica>> topMusicas = repository.getArtistTopMusics(nomeArtista);
                 topMusicas.ifPresentOrElse(
                         values -> values.forEach(m -> System.out.println(m.getNome())),
                         () -> System.out.println("Musicas mais populares não encontrado para " + nomeArtista)
+                );
+                break;
+            case LISTAR_ALBUNS_TIPO:
+                System.out.print("Quais tipos de album voce desejar listar (single/album): ");
+                String tipo = input.nextLine();
+                Optional<List <Album>> albunsTipo = repository.getAlbunsForType(tipo);
+                albunsTipo.ifPresentOrElse(
+                        values -> values.forEach(a -> System.out.println(a.getNomeAlbum())),
+                        () -> System.out.println("Não foram encontrados album desse tipo do artista " + nomeArtista)
+                );
+                break;
+            case LISTAR_ALBUNS_POR_NUMERO_FAIXAS:
+                Optional<List<Album>> albunsPorNumero = repository.getAlbunsTracksDesc();
+                albunsPorNumero.ifPresentOrElse(
+                        values -> values.forEach(a -> System.out.println(a.getNomeAlbum())),
+                        () -> System.out.println("Albuns de " + nomeArtista + "não encontrados")
+                );
+                break;
+            case LISTAR_NOME_ALBUNS:
+                Optional<List<Map<String, String>>> artistaNomeAlbum = repository.getArtistNameAndAlbumsName();
+                artistaNomeAlbum.ifPresentOrElse(
+                        values -> values.forEach(a -> {
+                                String artista = a.get("nome_artista");
+                                String album = a.get("nome_album");
+                            System.out.println(artista + " -> " + album);
+                        }), () -> System.out.println("Artista ou albuns não encontrados")
                 );
                 break;
             case SAIR:
